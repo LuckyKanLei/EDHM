6f042b0f9db256cc244f8ca8a7044218c19817e8
Kan Lei <mr_moment@outlook.com>
2021-04-11 16:21:56 +0200

    EVALUTE

--- R/CALIBRATE.R
+++ R/CALIBRATE.R
@@ -1,17 +1,22 @@
-#' #' @title Get an efficiency coefficient from the parameter group
-#' #' @import hydroGOF
-#' #' @param ParamterCalibrate parameter group
-#' #' @param fctEvaluate = NSE(Nash effect) The function to calculate the efficiency coefficient. It is recommended to be found in the "hydroGOF" R-package or written by yourself.
-#' #' @param InList InList for hydro Model
-#' #' @param Observe the observe data
-#' #' @param warmUPN = 730 the warm up period, they will not be evaluated
-#' #' @return coefficient Wert bei selecte coefficient function and Model
-#' #' @export
-#' CALIBRATE <- function(ParamterCalibrate, fctEvaluate = NSE, InList, Observe, warmUPN = 730){
-#'   class(ParamterCalibrate) <- class(InList)
-#'   PaList <- PaListMake(ParamterCalibrate)
-#'   Result <- MODELL(InList, PaList)
-#'   coefficientWert = fctEvaluate(Result[-(1: warmUPN),], as.matrix(Observe)[-(1: warmUPN),])
-#'   message("--------------------model end.\n", "Coefficient is: ", coefficientWert)
-#'   return(coefficientWert)
-#' }
+#' @title Get an efficiency coefficient from the parameter group
+#' @import hydroGOF
+#' @param Paramter_Calibrate parameter for the parameter that need to calibrate
+#' @param fct_Run_Model
+#' @param TimeVariData Time Vari Data
+#' @param TimeInvariData Time invariabel Data
+#' @param Paramter_fest parameter for the parameter that don't need to calibrate
+#' @param Observe the observe data
+#' @param warmUPN = 730 the warm up period, they will not be evaluated
+#' @param fct_Evaluate = NSE(Nash effect) The function to calculate the efficiency coefficient. It is recommended to be found in the "hydroGOF" R-package or written by yourself.
+#' @return coefficient Wert bei selecte coefficient function and Model
+#' @export
+EVALUATE <- function(Paramter_Calibrate, fct_Run_Model,
+                     TimeVariData, TimeInvariData, Paramter_fest,
+                     Observe,
+                     warmUPN = 300, fct_Evaluate = NSE){
+
+  Result_ <- fct_Run_Model(TimeVariData, TimeInvariData, Paramter_fest, Paramter_Calibrate)
+  coefficientWert = fct_Evaluate(Result_[-(1: warmUPN),], as.matrix(Observe)[-(1: warmUPN),])
+  message("--------------------model end.\n", "Coefficient is: ", coefficientWert)
+  return(coefficientWert)
+}
